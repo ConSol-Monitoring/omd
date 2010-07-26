1cf264d57483e194da2a5d2b7b76c62f9fd0507f
little <little@nagios.hallertau.net>
2010-07-26 14:23:15 +0200

    Added Arctic-Template to dokuwiki, changed admin to omdadmin, renamed hook

--- packages/dokuwiki/skel/etc/dokuwiki/local.php
+++ packages/dokuwiki/skel/etc/dokuwiki/local.php
@@ -3,4 +3,6 @@ $conf['title'] = 'OMD Dokuwiki ###SITE###';
 $conf['lang'] = 'en';
 $conf['useacl'] = 1;
 $conf['superuser'] = '@admin';
+$conf['template']='arctic';
+$conf['tpl']['arctic']['left_sidebar_content'] = 'index';
 ?>
--- packages/dokuwiki/skel/etc/dokuwiki/users.auth.php
+++ packages/dokuwiki/skel/etc/dokuwiki/users.auth.php
@@ -1,2 +1,2 @@
 # <?php exit()?>
-admin:$1$cce258b2$U9o5nK0z4MhTfB5QlKF23/:admin:admin@example.org:admin,user
+omdadmin:M29dfyFjgy5iA:OMD Admin:admin@example.org:admin,user
