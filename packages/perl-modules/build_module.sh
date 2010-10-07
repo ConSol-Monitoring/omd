e59914ed14831d80916c0499f6d2484cd862608d
Sven Nierlein <sven@nierlein.org>
2010-10-07 16:02:56 +0200

    changed order of modules && added missing ones

--- packages/perl-modules/build_module.sh
+++ packages/perl-modules/build_module.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
 
-PERL=$1
-MODULE=$2
-echo $MODULE
+MODULE=$1
+PERL=$2
+FORCE=$3
+
+LOG="install.log"
+echo "*** $MODULE" >&2
+
+#if [ -z $FORCE ]; then
+#    FORCE=0
+#fi
+if [ -z $PERL ]; then
+    PERL=/usr/bin/perl
+fi
 
 cd ..
 eval $($PERL -Idist/lib/perl5 -Mlocal::lib=dist)
 cd src
 
+#PMOD=`echo $MODULE | sed -e 's/\-[0-9]\..*gz//g' | sed -e 's/\-/::/g'`
+#$PERL -e "use $PMOD;" > /dev/null 2>&1
+#if [ $FORCE == 0 -a $? == 0 ]; then
+#  exit
+#fi
+
 tar zxf $MODULE
 dir=$(basename $MODULE | sed s/\.tar\.gz// )
 cd $dir
 if [ -f Build.PL ]; then
-    $PERL Build.PL
-    ./Build
-    ./Build install
+    $PERL Build.PL 2>&1 | tee -a $LOG | grep 'not found'
+    #if [ $? != 0 ]; then cat $LOG; exit 1; fi
+    ./Build >> $LOG 2>&1
+    #if [ $? != 0 ]; then cat $LOG; exit 1; fi
+    ./Build install >> $LOG 2>&1
+    #if [ $? != 0 ]; then cat $LOG; exit 1; fi
 elif [ -f Makefile.PL ]; then
-     echo "" | $PERL Makefile.PL
-     make
-     make install
+    echo "" | $PERL Makefile.PL 2>&1 | tee -a $LOG | grep 'not found'
+    #if [ $? != 0 ]; then cat $LOG; exit 1; fi
+    make >> $LOG 2>&1
+    #if [ $? != 0 ]; then cat $LOG; exit 1; fi
+    make install >> $LOG 2>&1
+    #if [ $? != 0 ]; then cat $LOG; exit 1; fi
 else
     echo "no Build.PL or Makefile.PL found in $MODULE!"
     exit 1
--- packages/perl-modules/download_package.sh
+++ packages/perl-modules/download_package.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-MODULE=$1
-
 cd src
-url=`wget -q -O - "http://search.cpan.org/perldoc?$MODULE" | sed 's/>/\n/g' | grep '/CPAN' | sed 's/.*"\(\/CPAN.*\.gz\)".*/\1/g' | head -n 1`
-url="http://search.cpan.org$url"
-echo "$url"
-wget "$url"
-
+for MODULE in $*; do
+    url=`wget -q -O - "http://search.cpan.org/perldoc?$MODULE" | sed 's/>/\n/g' | grep '/CPAN' | sed 's/.*"\(\/CPAN.*\.gz\)".*/\1/g' | head -n 1`
+    url="http://search.cpan.org$url"
+    echo "$url"
+    wget -q "$url"
+done
