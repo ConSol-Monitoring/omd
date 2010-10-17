18dc0b57ecc43194b55cb1b3173173168fe842f4
Sven Nierlein <sven@nierlein.de>
2010-10-17 16:54:42 +0200

    set perl path for thruk without local::lib

--- packages/thruk/skel/etc/thruk/thruk_env.pl
+++ packages/thruk/skel/etc/thruk/thruk_env.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/env perl
 
-# set omd environment
-use lib "###ROOT###/lib/perl5/lib/perl5";
-require local::lib; local::lib->import("###ROOT###/lib/perl5/");
+use Config;
 
-$ENV{'CATALYST_CONFIG'} ='###ROOT###/etc/thruk';
-$ENV{'PATH'} ='/usr/bin';
+# set omd environment
+$ENV{'PERL5LIB'}        = '###ROOT###/lib/perl5/lib/perl5/'.$Config{archname}.':###ROOT###/lib/perl5/lib/perl5';
+$ENV{'CATALYST_CONFIG'} = '###ROOT###/etc/thruk';
+$ENV{'PATH'}            = '/usr/bin';
+
+# execute fastcgi server
 exec '###ROOT###/share/thruk/script/thruk_fastcgi.pl';
