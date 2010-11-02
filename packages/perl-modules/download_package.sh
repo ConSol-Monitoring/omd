1e9cf1fcf39aa4aecf929ea3b53d5d841e121e7d
Sven Nierlein <sven@nierlein.de>
2010-11-02 23:38:38 +0100

    perl-modules: enhanced module download script

--- packages/perl-modules/download_package.sh
+++ packages/perl-modules/download_package.sh
@@ -2,7 +2,12 @@
 
 cd src
 for MODULE in $*; do
-    url=`wget -q -O - "http://search.cpan.org/perldoc?$MODULE" | sed 's/>/\n/g' | grep '/CPAN' | sed 's/.*"\(\/CPAN.*\.gz\)".*/\1/g' | head -n 1`
+    url=`wget -o wgetlog -O - "http://search.cpan.org/perldoc?$MODULE" | sed 's/>/\n/g' | grep '/CPAN' | sed 's/.*"\(\/CPAN.*\.gz\)".*/\1/g' | head -n 1`
+    if [ -z $url ]; then
+      echo "wget failed:"
+      cat wgetlog && rm wgetlog
+      exit 1
+    fi
     url="http://search.cpan.org$url"
     echo "$url"
     wget -q "$url"
