4b700a0c1a66d97592591e53cf19ca07edee40fb
Sven Nierlein <sven@nierlein.de>
2010-12-30 14:24:51 +0100

    thruk: fix for Date::Manip on centos with old perl

--- packages/thruk/skel/etc/thruk/thruk_env.pl
+++ packages/thruk/skel/etc/thruk/thruk_env.pl
@@ -5,7 +5,7 @@ use Config;
 # set omd environment
 $ENV{'PERL5LIB'}        = '###ROOT###/lib/perl5/lib/perl5/'.$Config{archname}.':###ROOT###/lib/perl5/lib/perl5';
 $ENV{'CATALYST_CONFIG'} = '###ROOT###/etc/thruk';
-$ENV{'PATH'}            = '/usr/bin';
+$ENV{'PATH'}            = '/bin:/usr/bin';
 
 # execute fastcgi server
 exec '###ROOT###/share/thruk/script/thruk_fastcgi.pl';
