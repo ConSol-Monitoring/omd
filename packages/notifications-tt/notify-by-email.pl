834da6bd69ed491fbdb6bf5aba4beec9d7b74ff1
Sven Nierlein <sven@nierlein.de>
2011-12-08 09:48:20 +0100

    notifications: added notification comment

--- packages/notifications-tt/notify-by-email.pl
+++ packages/notifications-tt/notify-by-email.pl
@@ -44,6 +44,7 @@ sub process_template {
 		$data .= "$_";
 	}
 	close FILE;
+	map($macro{$_} =~ s/\\n/\n/gmx, keys %macro);
 	my $template = Template->new({PRE_CHOMP => 1, POST_CHOMP => 0});
 	$template->process(\$data, \%macro, \$output);
 	print $output if $verbose;
