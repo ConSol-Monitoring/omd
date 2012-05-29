463df25d5522dce65d3711f2d922365a287a5a87
Bastian Kuhn <bk@mathias-kettner.de>
2012-05-29 10:26:08 +0200

    Dokuwiki: New Auth Module Mulisite (not as default)

--- packages/dokuwiki/skel/etc/dokuwiki/local.php
+++ packages/dokuwiki/skel/etc/dokuwiki/local.php
@@ -5,4 +5,6 @@ $conf['useacl'] = 1;
 $conf['superuser'] = '@admin';
 $conf['template']='arctic';
 $conf['tpl']['arctic']['left_sidebar_content'] = 'index';
+$conf['multisite']['authfile'] = '/omd/sites/###SITE###/var/check_mk/wato/auth/auth.php';
+$conf['multisite']['auth_secret'] = '/omd/sites/###SITE###/etc/auth.secret';
 ?>
