934d695794603d0530ae241e57bb297df51dd368
Joerg Linge <pitchfork@pnp4nagios.org>
2012-09-07 18:35:04 +0200

    icinga: config cleanup

--- packages/icinga/skel/etc/icinga/config.inc.php
+++ packages/icinga/skel/etc/icinga/config.inc.php
@@ -1,21 +1,21 @@
 <?php
 // This file exists per site and configures site specific paths for
-// Nagios' web pages
+// Icinga' web pages
 
 
-$cfg['cgi_config_file']='###ROOT###/etc/nagios/cgi.cfg';  // location of the CGI config file
+$cfg['cgi_config_file']='###ROOT###/etc/icinga/cgi.cfg';  // location of the CGI config file
 
-$cfg['cgi_base_url']='/###SITE###/nagios/cgi-bin';
+$cfg['cgi_base_url']='/###SITE###/icinga/cgi-bin';
 
 
 // FILE LOCATION DEFAULTS
-$cfg['main_config_file']='###ROOT###/tmp/nagios/nagios.cfg';  // default location of the main Nagios config file
-$cfg['status_file']='###ROOT###/tmp/nagios/status.dat'; // default location of Nagios status file
-$cfg['state_retention_file']='###ROOT###/spool/nagios/retention.dat'; // default location of Nagios retention file
+$cfg['main_config_file']='###ROOT###/tmp/icinga/icinga.cfg';          // default location of the main Icinga config file
+$cfg['status_file']='###ROOT###/tmp/icinga/status.dat';               // default location of Icinga status file
+$cfg['state_retention_file']='###ROOT###/spool/icinga/retention.dat'; // default location of Icinga retention file
 
 
 
 // utilities
-require_once('###ROOT###/share/nagios/htdocs/includes/utils.inc.php');
+require_once('###ROOT###/share/icinga/htdocs/includes/utils.inc.php');
 
 ?>
