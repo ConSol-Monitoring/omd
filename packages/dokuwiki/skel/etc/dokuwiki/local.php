8c499b830ba9bddc4bd283bb36d4481554863314
Matthias Gallinger <matthias.gallinger@consol.de>
2015-07-08 13:12:03 +0200

    Dokuwiki: Labs-Edition
    Signed-off-by: Sven Nierlein <Sven.Nierlein@consol.de>

--- packages/dokuwiki/skel/etc/dokuwiki/local.php
+++ packages/dokuwiki/skel/etc/dokuwiki/local.php
@@ -1,10 +1,9 @@
 <?php
-$conf['title'] = 'OMD Dokuwiki ###SITE###';
-$conf['lang'] = 'en';
-$conf['useacl'] = 1;
+$conf['title'] = 'OMD Labs-Edition Dokuwiki ###SITE###';
 $conf['license'] = 'cc-by-sa';
+$conf['useheading'] = '1';
+$conf['useacl'] = 1;
 $conf['superuser'] = '@admin';
-$conf['template']='arctictut';
 $conf['tpl']['arctictut']['sidebar'] = 'left';
 $conf['tpl']['arctictut']['left_sidebar_content'] = 'index';
 $conf['multisite']['authfile'] = '/omd/sites/###SITE###/var/check_mk/wato/auth/auth.php';
@@ -12,5 +11,3 @@ $conf['multisite']['auth_secret'] = '/omd/sites/###SITE###/etc/auth.secret';
 $conf['multisite']['auth_serials'] = '/omd/sites/###SITE###/etc/auth.serials';
 $conf['multisite']['htpasswd'] = '/omd/sites/###SITE###/etc/htpasswd';
 $conf['tpl']['vector']['vector_navigation_location'] = 'sidebar';
-
-?>
