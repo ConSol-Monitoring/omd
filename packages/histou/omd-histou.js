1f146c2ebc39177fd76f19d326e0184328f6978d
Philip Griesbacher <griesbacher@consol.de>
2015-09-24 10:47:34 +0200

    histou: added horizontal ruler

--- packages/histou/omd-histou.js
+++ packages/histou/omd-histou.js
@@ -9,46 +9,15 @@ parseArgs()
 
 return function(callback) {
 
-    //Remove Lines over Gap where are no points
-    (function ($) {
-        function init(plot) {
-            function fixGaps(plot, series, datapoints) {
-                    var points = datapoints.points, stepSize = datapoints.pointsize;
-                    var timerangeInMillisec = points[points.length-stepSize]-points[0];
-                    var datapoints = series.data.length;
-                    var avgTimeBetweenPoints = timerangeInMillisec / datapoints;
-                    var indices = [];
-                    for (var i = stepSize; i < points.length; i += stepSize){
-                            if ((points[i]-points[i-stepSize]) > avgTimeBetweenPoints*2){
-                                    indices.push(i)
-                            }
-                    }
-                    for (var i = 0; i < indices.length; i++){
-                            var pointIndex = indices[i];
-                            var oldTimestamp = points[pointIndex];
-                            for (var j = 0; i < stepSize; i++){
-                                    points.splice(pointIndex,0,null);
-                            }
-                    }
-            }
-            plot.hooks.processDatapoints.push(fixGaps);
-        }
-
-        $.plot.plugins.push({
-            init: init,
-            options: {},
-            name: "fixMeasurementGaps",
-            version: "0.1"
-        });
-    })(jQuery);
-
-
     site = window.location.href.match(/(https?:\/\/.*?\/.*?)\/grafana\/.*/);
     if(site.length > 1){
-        url = site[1]+'/histou/index.php';
+        url = site[1]+'/histou/';
     }
 
-    configUrl = url+'?host='+host+'&service='+service+'&height='+height+'&legend='+legend+debug;
+    flotAddons = url + 'flotAddons.js';
+    $.getScript(flotAddons, function(){});
+
+    configUrl = url+'index.php?host='+host+'&service='+service+'&height='+height+'&legend='+legend+debug;
 
     $.ajax({
         method: 'GET',
