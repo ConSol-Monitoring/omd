1f2c58812d75426052a6e9d08c7540151aaa37bd
Danijel Tasov <data@consol.de>
2021-01-19 09:17:06 +0100

    fix pymssql

--- packages/python-modules/has_cython.py
+++ packages/python-modules/has_cython.py
@@ -1,5 +1,8 @@
+import sys
 try:
     from Cython.Build import cythonize
+    print("cython found", file=sys.stderr)
     print(1)
 except Exception:
+    print("no cython found", file=sys.stderr)
     print(0)
