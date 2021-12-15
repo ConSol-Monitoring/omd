7e13087aaed4bd164cceb496009cda89526d23c9
Gerhard Lausser <gerhard.lausser@consol.de>
2021-12-15 14:28:55 +0100

    fix date pattern in check_traplog_alive.cfg

--- packages/snmptrapd/snmptrap_logger.sh
+++ packages/snmptrapd/snmptrap_logger.sh
@@ -10,12 +10,12 @@ if [ -z $LOGFILE ]; then
     exit 1
 fi
 DATE=`date`
+# dreckslinux1: Wed Dec 15 13:21:05 UTC 2021
+# dreckslinux2: Wed 15 Dec 2021 02:21:02 PM CET
 ALLINONE=""
 while read VAR; do
   NVAR=${VAR//[$'\t\r\n']}
-  #printf "[%s] %s\n" "$DATE" "$VAR" >> $LOGFILE
   printf "[%s] %s\n" "$DATE" "$NVAR" >> $LOGFILE
-  #ALLINONE="${ALLINONE}_,_$VAR"
   ALLINONE="${ALLINONE}____$NVAR"
 done
 printf "[%s] summary: %s\n" "$DATE" "$ALLINONE" >> $LOGFILE
