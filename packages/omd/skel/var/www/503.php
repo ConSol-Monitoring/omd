901cf778303f8563ffce32ceca2ecfbf14763360
Sven Nierlein <Sven.Nierlein@consol.de>
2023-07-12 08:57:20 +0200

    fix typo

--- packages/omd/skel/var/www/503.php
+++ packages/omd/skel/var/www/503.php
@@ -5,7 +5,7 @@
 foreach($_REQUEST as $key => $val) {
     if(isset($_SERVER["CONFIG_".$key])) {
         if($_SERVER["CONFIG_".$key] == $val) {
-            print "<h1>OMD: $key not available</h1>Service '".$key."' is not running. Run <code>omd start ".strtolower($key)." on</code> to start.";
+            print "<h1>OMD: $key not available</h1>Service '".$key."' is not running. Run <code>omd start ".strtolower($key)."</code> to start.";
         } else {
             print "<h1>OMD: $key not enabled</h1>Service '".$key."' is disabled. Run <code>omd config set ".$key." ".$val."</code> to enable.";
         }
