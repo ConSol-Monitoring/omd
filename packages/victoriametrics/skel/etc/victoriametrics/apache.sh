585f1c8b6ff4e1bc8aac5d5aa22611b8986c9319
Danijel Tasov <data@consol.de>
2023-07-14 19:48:08 +0200

    victoriametrics: always set VMPROTOCOL and VMUI_AUTH

--- packages/victoriametrics/skel/etc/victoriametrics/apache.sh
+++ packages/victoriametrics/skel/etc/victoriametrics/apache.sh
@@ -1,15 +1,14 @@
+export VMPROTOCOL=http
+export VMUI_AUTH=
 # Victoriametrics VMUI access
 if [ "$CONFIG_VICTORIAMETRICS" == "on" ]; then
   for CFG in etc/victoriametrics/conf.d/*.conf; do
     . $CFG
   done
 
-  export VMPROTOCOL=http
   [ $CONFIG_VICTORIAMETRICS_MODE = ssl ] && VMPROTOCOL=https
 
-  VMUI_AUTH=
   if [ "$vm_httpAuth_username" ] && [ "$vm_httpAuth_password" ] ; then
       VMUI_AUTH=$(printf "%s:%s" "$vm_httpAuth_username" "$vm_httpAuth_password" | base64)
   fi
-  export VMUI_AUTH
 fi
