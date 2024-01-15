3bc385f6b3a8d3f7d02aa9af0eebbb1485d2033c
Build Automat <build@localhost>
2024-01-15 08:50:14 +0100

    dokuwiki: index menu requires defer_js to be disabled

--- packages/dokuwiki/skel/etc/dokuwiki/local.php
+++ packages/dokuwiki/skel/etc/dokuwiki/local.php
@@ -3,6 +3,7 @@ $conf['title'] = 'OMD Labs-Edition Dokuwiki ###SITE###';
 $conf['license'] = 'cc-by-sa';
 $conf['useheading'] = '1';
 $conf['useacl'] = 1;
+$conf['defer_js'] = false;
 $conf['superuser'] = '@admin';
 $conf['tpl']['arctictut']['sidebar'] = 'left';
 $conf['tpl']['arctictut']['left_sidebar_content'] = 'index';
