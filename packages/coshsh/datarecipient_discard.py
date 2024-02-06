730016b7e7e93448a51da04f65d0f06ccfdb97bb
Gerhard Lausser <gerhard.lausser@consol.de>
2024-02-06 11:24:13 +0100

    add coshsh datasource/recipient_discard

---
+++ packages/coshsh/datarecipient_discard.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#-*- encoding: utf-8 -*-
+#
+# Copyright Gerhard Lausser.
+# This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+import logging
+import coshsh
+from coshsh.datarecipient import Datarecipient, DatarecipientNotAvailable
+from coshsh.util import compare_attr
+
+logger = logging.getLogger('coshsh')
+
+def __dr_ident__(params={}):
+    if compare_attr("type", params, "discard"):
+        return DrDiscard
+
+class DrDiscard(coshsh.datarecipient.Datarecipient):
+    def __init__(self, **kwargs):
+        self.name = kwargs["name"]
+
+    def read(self, filter=None, objects={}, force=False, **kwargs):
+        self.objects = objects
+
+    def output(self):
+        # this is only used to prevent the default recipient from kicking in
+        pass
+
---
+++ packages/coshsh/datasource_discard.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#-*- encoding: utf-8 -*-
+#
+# Copyright Gerhard Lausser.
+# This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+import logging
+import coshsh
+from coshsh.datasource import Datasource
+from coshsh.util import compare_attr
+
+logger = logging.getLogger('coshsh')
+
+def __ds_ident__(params={}):
+    if coshsh.util.compare_attr("type", params, "discard"):
+        return DsDiscard
+
+
+class DsDiscard(coshsh.datasource.Datasource):
+    def __init__(self, **kwargs):
+        super(self.__class__, self).__init__(**kwargs)
+
+    def read(self, filter=None, objects={}, force=False, **kwargs):
+        self.objects = objects
+        for k in self.objects.keys():
+            self.objects[k] = {}
