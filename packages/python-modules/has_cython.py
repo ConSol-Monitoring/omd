8655fb1deaa540c059fa40ae112c6053704b6050
Danijel Tasov <data@consol.de>
2025-03-05 13:31:06 +0100

    this was needed for sles12

--- packages/python-modules/has_cython.py
+++
@@ -1,8 +0,0 @@
-import sys
-try:
-    from Cython.Build import cythonize
-    print("cython found", file=sys.stderr)
-    print(1)
-except Exception:
-    print("no cython found", file=sys.stderr)
-    print(0)
